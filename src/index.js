1fac29eb444407aee8cc9163e1e9478b4da3dd93
vp5h <praveshvyas.18@gmail.com>
2022-02-14 13:28:18 +0530

    postcss-1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import './styles/index.scss'
+
 const elevenShieldRecipe = {
   leatherStrips: 2,
   ironIngot: 1,
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,34 @@
-let mode = 'development'
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-if(process.env.NODE_ENV === 'production'){
-    mode = 'production'
+MiniCssExtractPlugin;
+let mode = "development";
+
+if (process.env.NODE_ENV === "production") {
+  mode = "production";
 }
 
 module.exports = {
   mode: mode,
-  devtool: 'source-map',
+  devtool: "source-map",
 
   devServer: {
     static: "./dist",
+    hot: true
   },
-  module:{
-      rules: [
-         {
-             test: /\.js$/,
-             exclude: /node_modules/,
-             use: {
-                 loader: "babel-loader",
-             }
-        } 
-             
-      ]
-  }
+  module: {
+    rules: [
+      {
+        test: /\.s?css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", 'postcss-l','sass-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  plugins: [new MiniCssExtractPlugin()],
 };
