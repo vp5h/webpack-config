d4066d5fdd6ef4e15dd63d5d2f41fdabe0cace02
vp5h <praveshvyas.18@gmail.com>
2022-02-16 17:29:56 +0530

    image+rimraf

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,17 @@
 import "../styles/index.scss";
 import Recipes from "./Recipes";
+import sword from "../images/swc-sword.png";
+import swordSvg from "../images/sword.svg";
 
 const App = () => {
   return (
     <>
       <section className="hero">
-          <h1>Oh HI, React!!!</h1>
+        <h1>Oh HI, React!!!</h1>
       </section>
-      <Recipes/>
+        <img src={sword} alt="sword" width="250" />
+        <img src={swordSvg} alt="sword" width="250" />
+      <Recipes />
     </>
   );
 };
--- webpack.config.js
+++ webpack.config.js
@@ -15,12 +15,42 @@ module.exports = {
     static: "./dist",
     hot: true,
   },
+
+  output: {
+    assetModuleFilename: "images/[hash][ext][query]",
+  },
   module: {
     rules: [
       {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        /**
+         * The `type` setting replaces the need for "url-loader"
+         * and "file-loader" in Webpack 5.
+         *
+         * setting `type` to "asset" will automatically pick between
+         * outputing images to a file, or inlining them in the bundle as base64
+         * with a default max inline size of 8kb
+         */
+        type: "asset",
+
+        /**
+         * If you want to inline larger images, you can set
+         * a custom `maxSize` for inline like so:
+         */
+        // parser: {
+        //   dataUrlCondition: {
+        //     maxSize: 30 * 1024,
+        //   },
+        // },
+      },
+      {
         test: /\.(s[ac]|c)ss$/i,
         use: [
-          MiniCssExtractPlugin.loader,
+          {
+            loader: MiniCssExtractPlugin.loader,
+            // This is required for asset imports in CSS, such as url()
+            options: { publicPath: "" },
+          },
           "css-loader",
           "postcss-loader",
           "sass-loader",
@@ -37,6 +67,6 @@ module.exports = {
   },
   plugins: [new MiniCssExtractPlugin()],
   resolve: {
-    extensions: ['.js', '.jsx']
-  }
+    extensions: [".js", ".jsx"],
+  },
 };
